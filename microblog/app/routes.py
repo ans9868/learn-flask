5abd2fd7fb5f853c8ff01eb44aabfaafa4115e87
Nour999 <130527901+ans9868@users.noreply.github.com>
2023-11-22 23:34:22 -0500

    something no worky not sure why

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,18 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Jesse'}
-    posts = [
-        {
-            'author' : {'username' : 'jesse'},
-            'body': 'beautiful day in New Mexico'
-        },
-        {
-            'author': {'username': 'Mr White'},
-            'body': 'Indeed jesse, but the day is ripe for cooking'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
